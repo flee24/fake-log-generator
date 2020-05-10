eb270b11cc083fd18786d2cd716d409469eec311
Alexander R <coder.elf@gmail.com>
2020-05-11 00:32:03 +0300

    v0.1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const faker = require("faker");
 const cli = require("cli");
+const fs = require("fs");
+const fastcsv = require("fast-csv");
 
 cli.enable("status", "help");
 
@@ -7,3 +9,21 @@ const options = cli.parse({
   file: ["f", "Output file", "file", "main.log"],
   num: ["n", "Number of log lines", "int", 10000],
 });
+
+faker.seed(100);
+
+const data = [];
+for (i = 0; i < options.num; i++) {
+  data.push({
+    date: faker.date.past(),
+    uuid: faker.random.uuid(),
+    ip: faker.internet.ip(),
+    useragent: faker.internet.userAgent(),
+    url: faker.internet.url(),
+    username: faker.internet.userName(),
+  });
+}
+
+const outputStream = fs.createWriteStream(options.file);
+
+fastcsv.write(data, { headers: true }).pipe(outputStream);
